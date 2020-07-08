b9b42f5bc76458b3240657d51c20c6bc972ae8c7
LAPTOP-T0STR9M7\Akhil mohan <akhilmohan97@gmail.com>
2020-07-08 18:26:12 +0530

    button feature

---
+++ New Unity Project (1)/Assets/loadmaster.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class loadmaster : MonoBehaviour
+{
+    public Button button;
+    public string Master;
+    void Start()
+    {
+        button.onClick.AddListener(masterscene);
+    }
+
+    void masterscene ()
+    {
+        SceneManager.LoadScene(Master);
+
+    }
+   
+}
